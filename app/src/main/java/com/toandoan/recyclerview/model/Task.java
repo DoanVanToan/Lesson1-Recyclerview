e1224c8212fdfc37cc8742ee1663010831fc15f5
DoanVanToan <toandoan.coder@gmail.com>
2017-10-09 08:39:06 +0700

    Create Task Model

---
+++ app/src/main/java/com/toandoan/recyclerview/model/Task.java
@@ -0,0 +1,35 @@
+package com.toandoan.recyclerview.model;
+
+/**
+ * Created by toand on 10/9/2017.
+ */
+
+public class Task {
+    private int mId;
+    private String mTitle;
+    private String mDescription;
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+}
